03de79eb28515413a67c51ee0a532f00b1620778
Christopher Choi <chutsu@gmail.com>
2016-01-16 14:53:43 -0500

    Update hector_quad_simulator_install.bash

--- scripts/hector_quad_simulator_install.bash
+++ scripts/hector_quad_simulator_install.bash
@@ -1,22 +1,23 @@
 #!/bin/sh
+HECTOR_PATH=$HOME
 
 install_dependencies()
 {
     apt-get install -y \
         ros-indigo-desktop-full
 }
+
 install_hector_sim()
 {
-    cd $HOME
-    mkdir ~/hector_quadrotor_tutorial
-    cd ~/hector_quadrotor_tutorial
+    cd $HECTOR_PATH
+    mkdir $HECTOR_PATH/hector_quadrotor_tutorial
+    cd $HECTOR_PATH/hector_quadrotor_tutorial
     wstool init src https://raw.github.com/tu-darmstadt-ros-pkg/hector_quadrotor/hydro-devel/tutorials.rosinstall
     catkin_make -j2
     source devel/setup.bash
 }
 
+
 #RUN
 install_dependencies
 install_hector_sim
-
-cd $HOME
