c12b4994eda2cd58ffee6d45577b853b7aabcd21
Chris Choi <chutsu@gmail.com>
2016-01-26 00:58:04 -0600

    Update ardupilot install script

--- scripts/ardupilot_install.sh
+++ scripts/ardupilot_install.sh
@@ -1,30 +1,41 @@
 #!/bin/bash
+set -e  # halt on first error
 AWESOMO_BUILD_PATH=$HOME/awesomo_build/
 AWESOMO_CATKIN_PATH=$HOME/awesomo_catkin/
 
 
 install_dependencies()
 {
+    # install required ros packages
+    sudo apt-get install -y \
+	ros-indigo-octomap-msgs \
+	python-rosdep
+
+    sudo rm /etc/ros/rosdep/sources.list.d/20-default.list
+    sudo rosdep init
+    sudo rosdep update
+    sudo rosdep fix-permissions
+
     # create catkin workspace
+    source /opt/ros/indigo/setup.bash
     mkdir -p $AWESOMO_CATKIN_PATH/src
     cd $AWESOMO_CATKIN_PATH/src
     catkin_init_workspace
+    cd $AWESOMO_CATKIN_PATH
+    catkin_make
     source devel/setup.bash
-
-    # install required ros packages
-    sudo apt-get install ros-indigo-octomap-msgs
 }
 
 install_ardupilot()
 {
     # clone ardupilot
-    cd $AWESOMO_BUILD_PATH
     mkdir -p $AWESOMO_BUILD_PATH
+    cd $AWESOMO_BUILD_PATH
     git clone https://github.com/alexbuyval/ardupilot
+    cd ardupilot
     git checkout RangeFinderSITL2
 
     # clone all necessary ROS packages
-    roscd
     cd $AWESOMO_CATKIN_PATH/src
     git clone https://alexbuyval@bitbucket.org/alexbuyval/arducopter_sitl_ros.git
     git clone https://github.com/PX4/mav_comm.git
@@ -35,7 +46,7 @@ install_ardupilot()
     git checkout sonar_plugin
     cd ../..
     wstool init src
-    wstool set -t src mavros --git https://github.com/alexbuyval/mavros.git
+    yes | wstool set -t src mavros --git https://github.com/alexbuyval/mavros.git
     wstool update -t src
     rosdep install --from-paths src --ignore-src --rosdistro indigo -y
 
@@ -50,3 +61,8 @@ start_sim()
     cd $AWESOMO_BUILD_PATH/ardupilot/ArduCopter
     sim_vehicle.sh -f arducopter_sitl_ros --console
 }
+
+
+# RUN
+install_dependencies
+install_ardupilot
