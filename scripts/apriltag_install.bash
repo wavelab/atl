a24cbdb53e96a3226b320f3c7950053ed33c6db9
Christopher Choi <chutsu@gmail.com>
2016-01-25 19:14:54 -0500

    Create apriltag_install.bash

---
+++ scripts/apriltag_install.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+install_dependencies()
+{
+    # install dependencies
+    apt-get install -y \
+        subversion \
+        cmake \
+        libopencv-dev \
+        libeigen3-dev \
+        libv4l-dev
+}
+
+install_apriltags()
+{
+    cd $HOME
+    svn co https://svn.csail.mit.edu/apriltags
+    cd apriltags && make
+    cp -R build/include/AprilTags /usr/include/
+    cp -R build/lib/libapriltags.a /usr/lib/
+    cd $HOME
+    rm -rf apriltags
+}
+
+
+# RUN
+install_dependencies
+install_apriltags
