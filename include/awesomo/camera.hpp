aad0b26c5b09019fb2de455a6e3d3c7508a7db06
Chris Choi <chutsu@gmail.com>
2016-02-01 19:00:35 -0500

    Add apriltag pose estimation - but needs calibration

--- include/awesomo/camera.hpp
+++ include/awesomo/camera.hpp
@@ -1,7 +1,15 @@
 #ifndef __WEBCAM_H__
 #define __WEBCAM_H__
 
+#define _USE_MATH_DEFINES
+#ifdef M_PI
+  #define TWOPI 2*M_PI
+#else
+  #define TWOPI 2.0*3.1415926535897932384626433832795
+#endif
+
 #include <iostream>
+#include <math.h>
 #include <sys/time.h>
 
 #include <opencv2/imgproc/imgproc.hpp>
@@ -16,12 +24,26 @@ class Camera
         AprilTags::TagDetector* tag_detector;
 
         void printFPS(double &last_tic, int &frame);
+        float calculateFocalLength(void);
+        double standardRad(double t);
+        void convertToEuler(
+            const Eigen::Matrix3d &wRo,
+            double &yaw,
+            double &pitch,
+            double &roll
+        );
+        void printDetection(AprilTags::TagDetection& detection);
         int processImage(cv::Mat &image, cv::Mat &image_gray);
 
     public:
         int camera_index;
         int image_width;
         int image_height;
+
+        float focal_length;
+        float field_of_view;
+        float ccd_width;
+
         vector<AprilTags::TagDetection> apriltags;
 
         Camera(void);
--- src/camera.cpp
+++ src/camera.cpp
@@ -8,12 +8,19 @@ static double tic(void)
     return ((double) t.tv_sec + ((double) t.tv_usec) / 1000000.0);
 }
 
+
 Camera::Camera(void)
 {
-    this->camera_index = 1;
+    this->camera_index = 0;
     this->image_width = 400;
     this->image_height = 400;
+
+    this->focal_length = 4.0;
+    this->field_of_view = 60;
+    // this->ccd_width = NULL;
+
     this->tag_detector = new AprilTags::TagDetector(AprilTags::tagCodes16h5);
+
 }
 
 void Camera::printFPS(double &last_tic, int &frame)
@@ -62,30 +69,116 @@ int Camera::run(void)
     return 0;
 }
 
+float Camera::calculateFocalLength(void)
+{
+    if (this->ccd_width) {
+        return this->image_width * this->focal_length / this->ccd_width;
+
+    } else if (this->field_of_view) {
+        return (this->image_width * 0.5) / tan(this->field_of_view * 0.5 * M_PI / 180);
+
+    } else {
+        return -1;
+
+    }
+}
+
+double Camera::standardRad(double t)
+{
+    // normalize angle to be within the interval [-pi,pi].
+    if (t >= 0.) {
+        t = fmod(t + M_PI, TWOPI) - M_PI;
+    } else {
+        t = fmod(t - M_PI, -TWOPI) + M_PI;
+    }
+
+    return t;
+}
+
+void Camera::convertToEuler(
+    const Eigen::Matrix3d &wRo,
+    double &yaw,
+    double &pitch,
+    double &roll
+)
+{
+    yaw = this->standardRad(atan2(wRo(1,0), wRo(0,0)));
+    double c = cos(yaw);
+    double s = sin(yaw);
+    pitch = this->standardRad(atan2(-wRo(2,0), wRo(0,0)*c + wRo(1,0)*s));
+    roll  = this->standardRad(atan2(wRo(0,2)*s - wRo(1,2)*c, -wRo(0,1)*s + wRo(1,1)*c));
+}
+
+void Camera::printDetection(AprilTags::TagDetection& detection)
+{
+    double yaw;
+    double pitch;
+    double roll;
+
+    double m_tag_size;
+    double m_fx;
+    double m_fy;
+    double m_px;
+    double m_py;
+
+    Eigen::Vector3d translation;
+    Eigen::Matrix3d rotation;
+    Eigen::Matrix3d F;
+    Eigen::Matrix3d fixed_rot;
+
+    // setup
+    F << 1, 0, 0,
+         0, -1, 0,
+         0, 0, 1;
+
+    m_tag_size = 0.343;
+    m_fx = this->calculateFocalLength();
+    m_fy = this->calculateFocalLength();
+    m_px = this->image_width / 2.0;
+    m_py = this->image_height / 2.0;
+
+    // recovering the relative pose of a tag:
+    detection.getRelativeTranslationRotation(
+        m_tag_size,
+        m_fx,
+        m_fy,
+        m_px,
+        m_py,
+        translation,
+        rotation
+    );
+
+    fixed_rot = F * rotation;
+    this->convertToEuler(fixed_rot, yaw, pitch, roll);
+
+    std::cout << "id: " << detection.id << " ";
+    std::cout << "Hamming: " << detection.hammingDistance << ")";
+    std::cout << "  distance=" << translation.norm() << "m ";
+    std::cout << "x=" << translation(0) << ", ";
+    std::cout << "y=" << translation(1) << ", ";
+    std::cout << "z=" << translation(2);
+    std::cout << "yaw=" << yaw << ", ";
+    std::cout << "pitch=" << pitch << ", ";
+    std::cout << "roll=" << roll << ", ";
+    std::cout << endl;
+
+    // also note that for SLAM/multi-view application it is better to
+    // use reprojection error of corner points, because the noise in
+    // this relative pose is very non-Gaussian; see iSAM source code
+    // for suitable factors.
+}
+
 int Camera::processImage(cv::Mat &image, cv::Mat &image_gray)
 {
-    cv::Mat image_bw;
-    int bw_threshold;
-
     // detect april tags (requires a gray scale image)
     cv::cvtColor(image, image_gray, CV_BGR2GRAY);
-    bw_threshold = 128;
-    image_bw = image_gray > bw_threshold;
-    // cv::adaptiveThreshold(
-    //     image_gray,
-    //     image_gray,
-    //     255,
-    //     CV_ADAPTIVE_THRESH_GAUSSIAN_C,
-    //     CV_THRESH_BINARY,
-    //     15,
-    //     -5
-    // );
-    //
-    cv::imshow("camera", image_bw);
-    this->apriltags = this->tag_detector->extractTags(image_bw);
+    cv::imshow("camera", image_gray);
+    this->apriltags = this->tag_detector->extractTags(image_gray);
 
     // print out each apriltags
-    std::cout << this->apriltags.size() << " tags detected:" << endl;
+    if (this->apriltags.size()) {
+        this->printDetection(this->apriltags[0]);
+    }
 
     return this->apriltags.size();
 }
