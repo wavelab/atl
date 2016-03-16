13656845a699655ca88e43d0e9ce177f7557d41d
Chris Choi <chutsu@gmail.com>
2016-03-16 14:56:50 -0400

    Fix pointgrey install script

--- scripts/install_scripts/pointgrey_install.bash
+++ scripts/install_scripts/pointgrey_install.bash
@@ -2,8 +2,20 @@
 set -e #halt on first error
 
 # get dir that the script is located in
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+echo $DIR
+
 install_pointgrey_drivers(){
+    # pointgrey driver dependencies
+    sudo apt-get install \
+        libraw1394-11 \
+        libgtkmm-2.4-1c2a \
+        libglademm-2.4-1c2a \
+        libgtkglextmm-x11-1.2-dev \
+        libgtkglextmm-x11-1.2 \
+        libusb-1.0-0 \
+        -y
+
     # unzip drivers
     cd $DIR
     cd ../../deps
@@ -16,10 +28,8 @@ install_pointgrey_drivers(){
 
 # install coriander for setting camera configurations
 install_coriander(){
-    sudo apt-get install coriander
+    sudo apt-get install coriander -y
 }
 
 install_pointgrey_drivers
 install_coriander
-
-
