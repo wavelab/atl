de2a1c6b62bfa956e12e54cfab35dfa9135f8e67
Chris Choi <chutsu@gmail.com>
2016-05-06 17:55:18 -0400

    Add navio scripts

---
+++ scripts/navio/apm_install.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget "https://files.emlid.com/apm/apm-navio2_3.4-unstable.deb" -O apm-navio2.deb
+sudo dpkg -i apm-navio2.deb
---
+++ scripts/navio/apm_launch.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo ArduCopter-quad -A udp:192.168.1.145:14550
