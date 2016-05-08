d315bbf7ff9075abfe37944f097e263c0fea5d08
Chris Choi <chutsu@gmail.com>
2016-05-08 18:47:48 -0400

    Moving away from ROS - YAY~

--- scripts/install_scripts/dependencies.bash
+++ scripts/install_scripts/dependencies.bash
@@ -3,6 +3,4 @@ set -e  # halt on first error
 
 #bash ros_install.bash
 bash apriltag_install.bash
-bash hector_quad_simulator_install.bash
-bash husky_simulator_install.bash
 bash quadsim_install.bash
