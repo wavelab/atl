2a9ae0c3ef07c0349d4d6bca68d35ab9d4b6a9ce
Chris Choi <chutsu@gmail.com>
2016-05-08 15:33:16 -0400

    Add navio library code

---
+++ include/awesomo/imu.hpp
@@ -0,0 +1,10 @@
+#ifndef __IMU_HPP__
+#define __IMU_HPP__
+
+#include <stdlib.h>
+
+#include "navio/MPU9250.h"
+#include "navio/Util.h"
+
+
+#endif
---
+++ src/imu.cpp
@@ -0,0 +1,27 @@
+#include "awesomo/imu.hpp"
+
+
+// int main()
+// {
+//     if (check_apm()) {
+//         return 1;
+//     }
+//
+// 	MPU9250 imu;
+// 	imu.initialize();
+//
+// 	float ax, ay, az;
+// 	float gx, gy, gz;
+// 	float mx, my, mz;
+//
+//     while (1) {
+//         imu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+// 		printf("Acc: %+7.3f %+7.3f %+7.3f  ", ax, ay, az);
+// 		printf("Gyr: %+8.3f %+8.3f %+8.3f  ", gx, gy, gz);
+// 		printf("Mag: %+7.3f %+7.3f %+7.3f\n", mx, my, mz);
+//
+// 		usleep(500000);
+//     }
+//
+// 	return 0;
+// }
