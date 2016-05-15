4c4a8c3fd3f22a0dd6166e35742188dc036a50c8
Stan Brown <stan.j.brown@gmail.com>
2016-05-15 17:25:13 -0400

    working on ekf

--- include/ekf.hpp
+++ include/ekf.hpp
@@ -2,14 +2,12 @@
 #define __EKF_HPP__
 
 #include <iostream>
-
 #include <Eigen/Dense>
 
 
 // STRUCTURES
 struct ekf
 {
-    double dt;
 
     Eigen::VectorXd mu;
     Eigen::MatrixXd S;
@@ -21,16 +19,27 @@ struct ekf
     Eigen::MatrixXd K;
     Eigen::MatrixXd I;
 
-    Eigen::VectorXd (*g_function)(Eigen::VectorXd &, Eigen::VectorXd &, double);
-    Eigen::MatrixXd (*G_function)(Eigen::VectorXd &, Eigen::VectorXd &, double);
-    Eigen::VectorXd (*h_function)(Eigen::VectorXd &, double);
-    Eigen::MatrixXd (*H_function)(Eigen::VectorXd &, double);
+    Eigen::VectorXd (*g_function)(Eigen::VectorXd &, Eigen::VectorXd &, float);
+    Eigen::MatrixXd (*G_function)(Eigen::VectorXd &, Eigen::VectorXd &, float);
+    Eigen::VectorXd (*h_function)(Eigen::VectorXd &, float);
+    Eigen::MatrixXd (*H_function)(Eigen::VectorXd &, float);
 };
-
 
 // FUNCTIONS
 int ekf_check(struct ekf *estimator);
-void ekf_prediction_update(struct ekf *estimator, Eigen::VectorXd u);
-void ekf_measurement_update(struct ekf *estimator, Eigen::VectorXd y);
+void ekf_prediction_update(struct ekf *estimator, Eigen::VectorXd u, float dt);
+void ekf_measurement_update(struct ekf *estimator, Eigen::VectorXd y, float dt);
+
+
+// Attitude EKF
+Eigen::VectorXd att_g_function(Eigen::VectorXd &mu_p, Eigen::VectorXd &u,
+                               float dt);
+Eigen::MatrixXd att_G_function(Eigen::VectorXd &mu_p, Eigen::VectorXd &u,
+                               float dt);
+Eigen::VectorXd att_h_function(Eigen::VectorXd &y, float dt);
+Eigen::MatrixXd att_H_function(Eigen::VectorXd &mu_p, float dt);
+
+int initilize_att_ekf(ekf *att_ekf, Eigen::MatrixXd &R, Eigen::MatrixXd &Q,
+                      Eigen::VectorXd mu_init);
 
 #endif
