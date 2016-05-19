885ff8cb629ab5eaea447ad8eff71f5bc87b45f5
Chris Choi <chutsu@gmail.com>
2016-05-19 15:02:11 -0400

    Remove useless junk

--- include/awesomo/navio/Util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
-
-int write_file(const char *path, const char *fmt, ...);
-int read_file(const char *path, const char *fmt, ...);
-bool check_apm();
