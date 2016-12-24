1fb1a32d3ab9900c7cdaf02e9fddc76cb3f4ba74
Chris Choi <chutsu@gmail.com>
2016-12-23 20:22:11 -0500

    Add tracking controller with tests and configs

---
+++ awesomo_core/include/awesomo_core/control/tracking_controller.hpp
@@ -0,0 +1,45 @@
+#ifndef __AWESOMO_CONTROL_TRACKING_CONTROLLER_HPP__
+#define __AWESOMO_CONTROL_TRACKING_CONTROLLER_HPP__
+
+#include <yaml-cpp/yaml.h>
+
+#include "awesomo_core/utils/utils.hpp"
+#include "awesomo_core/control/pid_controller.hpp"
+
+
+namespace awesomo {
+
+class TrackingController {
+public:
+  bool configured;
+
+  PID x_controller;
+  PID y_controller;
+  PID z_controller;
+
+  double hover_throttle;
+
+  double roll_limit[2];
+  double pitch_limit[2];
+
+  double setpoint_x;
+  double setpoint_y;
+  double setpoint_z;
+
+  double output_roll;
+  double output_pitch;
+  double output_throttle;
+
+  AttitudeCommand att_cmd;
+
+  TrackingController(void);
+  int configure(std::string config_file);
+  VecX calculate(VecX setpoints, VecX actual, double yaw, double dt);
+  void reset(void);
+  void printErrors(void);
+  void printInputs(void);
+  void printOutputs(void);
+};
+
+}  // end of awesomo namespace
+#endif
---
+++ awesomo_core/tests/control/tracking_controller_test.cpp
@@ -0,0 +1,158 @@
+#include <gtest/gtest.h>
+
+#include "awesomo_core/control/tracking_controller.hpp"
+
+
+#define TEST_CONFIG "tests/configs/tracking_controller.yaml"
+
+
+namespace awesomo {
+
+TEST(TrackingController, constructor) {
+  TrackingController controller;
+
+  ASSERT_FALSE(controller.configured);
+
+  ASSERT_FLOAT_EQ(0.0, controller.x_controller.k_p);
+  ASSERT_FLOAT_EQ(0.0, controller.x_controller.k_i);
+  ASSERT_FLOAT_EQ(0.0, controller.x_controller.k_d);
+
+  ASSERT_FLOAT_EQ(0.0, controller.y_controller.k_p);
+  ASSERT_FLOAT_EQ(0.0, controller.y_controller.k_i);
+  ASSERT_FLOAT_EQ(0.0, controller.y_controller.k_d);
+
+  ASSERT_FLOAT_EQ(0.0, controller.z_controller.k_p);
+  ASSERT_FLOAT_EQ(0.0, controller.z_controller.k_i);
+  ASSERT_FLOAT_EQ(0.0, controller.z_controller.k_d);
+
+  ASSERT_FLOAT_EQ(0.0, controller.hover_throttle);
+
+  ASSERT_FLOAT_EQ(0.0, controller.roll_limit[0]);
+  ASSERT_FLOAT_EQ(0.0, controller.roll_limit[1]);
+
+  ASSERT_FLOAT_EQ(0.0, controller.pitch_limit[0]);
+  ASSERT_FLOAT_EQ(0.0, controller.pitch_limit[1]);
+
+  ASSERT_FLOAT_EQ(0.0, controller.setpoint_x);
+  ASSERT_FLOAT_EQ(0.0, controller.setpoint_y);
+  ASSERT_FLOAT_EQ(0.0, controller.setpoint_z);
+
+  ASSERT_FLOAT_EQ(0.0, controller.output_roll);
+  ASSERT_FLOAT_EQ(0.0, controller.output_pitch);
+  ASSERT_FLOAT_EQ(0.0, controller.output_throttle);
+}
+
+TEST(TrackingController, configure) {
+  TrackingController controller;
+
+  controller.configure(TEST_CONFIG);
+
+  ASSERT_TRUE(controller.configured);
+
+  ASSERT_FLOAT_EQ(0.1, controller.x_controller.k_p);
+  ASSERT_FLOAT_EQ(0.2, controller.x_controller.k_i);
+  ASSERT_FLOAT_EQ(0.3, controller.x_controller.k_d);
+
+  ASSERT_FLOAT_EQ(0.1, controller.y_controller.k_p);
+  ASSERT_FLOAT_EQ(0.2, controller.y_controller.k_i);
+  ASSERT_FLOAT_EQ(0.3, controller.y_controller.k_d);
+
+  ASSERT_FLOAT_EQ(0.1, controller.z_controller.k_p);
+  ASSERT_FLOAT_EQ(0.2, controller.z_controller.k_i);
+  ASSERT_FLOAT_EQ(0.3, controller.z_controller.k_d);
+
+  ASSERT_FLOAT_EQ(0.6, controller.hover_throttle);
+
+  ASSERT_FLOAT_EQ(deg2rad(-50.0), controller.roll_limit[0]);
+  ASSERT_FLOAT_EQ(deg2rad(50.0), controller.roll_limit[1]);
+
+  ASSERT_FLOAT_EQ(deg2rad(-50.0), controller.pitch_limit[0]);
+  ASSERT_FLOAT_EQ(deg2rad(50.0), controller.pitch_limit[1]);
+
+  ASSERT_FLOAT_EQ(0.0, controller.setpoint_x);
+  ASSERT_FLOAT_EQ(0.0, controller.setpoint_y);
+  ASSERT_FLOAT_EQ(0.0, controller.setpoint_z);
+
+  ASSERT_FLOAT_EQ(0.0, controller.output_roll);
+  ASSERT_FLOAT_EQ(0.0, controller.output_pitch);
+  ASSERT_FLOAT_EQ(0.0, controller.output_throttle);
+}
+
+TEST(TrackingController, calculate) {
+  Vec3 setpoint, actual;
+  float yaw_setpoint, dt;
+  TrackingController controller;
+
+  // setup
+  controller.configure(TEST_CONFIG);
+
+  // CHECK HOVERING PID OUTPUT
+  setpoint << 0, 0, 0;
+  actual << 0, 0, 0;
+  yaw_setpoint = 0;
+  dt = 0.1;
+  controller.calculate(setpoint, actual, yaw_setpoint, dt);
+  controller.printOutputs();
+
+  ASSERT_FLOAT_EQ(0.0, controller.output_roll);
+  ASSERT_FLOAT_EQ(0.0, controller.output_pitch);
+  ASSERT_FLOAT_EQ(controller.hover_throttle, controller.output_throttle);
+
+  // CHECK MOVING TOWARDS THE Y LOCATION
+  setpoint << 0, 1, 0;
+  actual << 0, 0, 0;
+  yaw_setpoint = 0;
+  dt = 0.1;
+
+  controller.reset();
+  controller.calculate(setpoint, actual, yaw_setpoint, dt);
+  controller.printOutputs();
+
+  ASSERT_TRUE(controller.output_roll < 0.0);
+  ASSERT_FLOAT_EQ(0.0, controller.output_pitch);
+
+  // CHECK MOVING TOWARDS THE X LOCATION
+  setpoint << 1, 0, 0;
+  actual << 0.0, 0, 0;
+  yaw_setpoint = 0;
+  dt = 0.01;
+
+  controller.reset();
+  controller.calculate(setpoint, actual, yaw_setpoint, dt);
+  controller.printOutputs();
+
+  ASSERT_FLOAT_EQ(0.0, controller.output_roll);
+  ASSERT_TRUE(controller.output_pitch > 0.0);
+
+  // CHECK MOVING TOWARDS THE X AND Y LOCATION
+  setpoint << 1, 1, 0;
+  actual << 0, 0, 0;
+  yaw_setpoint = 0;
+  dt = 0.1;
+
+  controller.reset();
+  controller.calculate(setpoint, actual, yaw_setpoint, dt);
+  controller.printOutputs();
+
+  ASSERT_TRUE(controller.output_roll < 0.0);
+  ASSERT_TRUE(controller.output_pitch > 0.0);
+
+  // CHECK MOVING YAW
+  setpoint << 0, 0, 0;
+  actual << 0, 0, 0;
+  yaw_setpoint = deg2rad(90.0);
+  dt = 0.1;
+
+  controller.reset();
+  Vec4 outputs = controller.calculate(setpoint, actual, yaw_setpoint, dt);
+  controller.printOutputs();
+
+  ASSERT_FLOAT_EQ(yaw_setpoint, outputs(2));
+}
+
+}  // end of awesomo namepsace
+
+int main(int argc, char *argv[]) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
