3813061666550074e14ea5915e80dea302a0b4d8
Chris Choi <chutsu@gmail.com>
2016-12-28 18:55:38 -0500

    Do not install ximea drivers in travis

--- scripts/install/awesomo_install.bash
+++ scripts/install/awesomo_install.bash
@@ -8,7 +8,7 @@ REPO_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)"
 sudo -v
 
 # intall dependencies
-sudo bash $SCRIPT_PATH/ximea_install.bash
+# sudo bash $SCRIPT_PATH/ximea_install.bash
 sudo bash $SCRIPT_PATH/pointgrey_install.bash
 sudo bash $SCRIPT_PATH/swathmore_apriltags.bash
 
