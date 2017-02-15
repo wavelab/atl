9782bae5a3d4b83512f13edbd94f658bf1c0ef66
Chris Choi <chutsu@gmail.com>
2017-02-15 18:28:01 -0500

    Add old i2c and MPU6050 code

---
+++ awesomo_core/include/awesomo_core/sensor/i2c.hpp
@@ -0,0 +1,41 @@
+#ifndef __AWESOMO_CORE_SENSOR_I2C_HPP__
+#define __AWESOMO_CORE_SENSOR_I2C_HPP__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/ioctl.h>
+
+#include <linux/i2c-dev.h>
+
+
+namespace awesomo {
+
+// ERROR MESSAGES
+#define I2C_INIT_FAILED "Failed to initialize I2C!"
+
+// DEFINES
+#define I2C_BUF_MAX 1024
+
+class I2C
+{
+public:
+  int fd;
+
+  I2C(void);
+  ~I2C(void);
+  int setup(void);
+  int setSlave(char slave_addr);
+  int readBytes(char reg_addr, char *data, size_t length);
+  int readByte(char reg_addr, char *data);
+  int writeByte(char reg_addr, char byte);
+  int writeRawByte(char byte);
+  int writeBytes(char reg_addr, char *data, size_t length);
+};
+
+}  // end of awesomo namespace
+#endif
---
+++ awesomo_core/src/sensor/i2c.cpp
@@ -0,0 +1,108 @@
+#include "awesomo_core/sensor/i2c.hpp"
+
+
+namespace awesomo {
+
+I2C::I2C(void) {
+  this->fd = -1;
+}
+
+I2C::~I2C(void) {
+  close(this->fd);
+}
+
+int I2C::setup(void) {
+  int fd;
+  int adapter_nr;
+  char filename[20];
+
+  /* setup */
+  adapter_nr = 1;  /* probably dynamically determined */
+  memset(filename, '\0', sizeof(char) * 20);
+  snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
+
+  /* open i2c connection */
+  fd = open(filename, O_RDWR);
+  if (fd < 0) {
+    return -1;
+  } else {
+    this->fd = fd;
+  }
+
+  return 0;
+}
+
+int I2C::setSlave(char slave_addr) {
+  return ioctl(this->fd, I2C_SLAVE , slave_addr);
+}
+
+int I2C::readByte(char reg_addr, char *data) {
+  char buf[1];
+
+  buf[0] = reg_addr;
+  if (write(this->fd, buf, 1) != 1) {
+    return -1;
+  }
+
+  if (read(this->fd, data, 1) != 1) {
+    return -2;
+  }
+
+  return 0;
+}
+
+int I2C::readBytes(char reg_addr, char *data, size_t length) {
+  char buf[1];
+
+  buf[0] = reg_addr;
+  if (write(this->fd, buf, 1) != 1) {
+    return -1;
+  }
+
+  if (read(this->fd, data, length) != (int) length) {
+    return -2;
+  }
+
+  return 0;
+}
+
+int I2C::writeByte(char reg_addr, char byte) {
+  char buf[2];
+
+  buf[0] = reg_addr;
+  buf[1] = byte;
+  if (write(this->fd, buf, 2) != 1) {
+    return -1;
+  }
+
+  return 0;
+}
+
+int I2C::writeRawByte(char byte) {
+  if (write(this->fd, &byte, 1) != 1) {
+    return -1;
+  }
+
+  return 0;
+}
+
+int I2C::writeBytes(char reg_addr, char *data, size_t length) {
+  int i;
+  char buf[I2C_BUF_MAX];
+
+  /* create buf */
+  memset(buf, '\0', sizeof(char) * I2C_BUF_MAX);
+  buf[0] = reg_addr;
+  for (i = 1; i < (int) length + 1; i++) {
+    buf[i] = data[i];
+  }
+
+  /* write bytes */
+  if (write(this->fd, buf, length + 1) != 1) {
+    return -1;
+  }
+
+  return 0;
+}
+
+}  // end of awesomo namespace
