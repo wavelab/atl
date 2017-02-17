3f39b86f9fa848be39932b31739b64aa7cad0437
Stan Brown <stan.j.brown@gmail.com>
2017-02-17 13:31:10 -0500

    Update Point Grey Camera class

--- awesomo_core/include/awesomo_core/vision/camera/pointgrey.hpp
+++ awesomo_core/include/awesomo_core/vision/camera/pointgrey.hpp
@@ -10,6 +10,7 @@ namespace awesomo {
 class PointGreyCamera : public Camera {
 public:
   FlyCapture2::Camera *pointgrey;
+  double shutter_speed = 0.5;
 
   PointGreyCamera(void);
   ~PointGreyCamera(void);
@@ -19,11 +20,27 @@ public:
                  int crop_width,
                  int crop_height,
                  int mode);
-  int setFrameRate(double frame_rate);
-  int setExposure(double exposure);
-  int setGain(double gain);
+  int setFormat7VideoMode(int format7_mode,
+                 std::string pixel_format,
+                 int width,
+                 int height,
+                 bool center_ROI);
+  int setROI(const FlyCapture2::Format7Info &format7_info,
+                             FlyCapture2::Format7ImageSettings &format7_settings,
+                             int width,
+                             int height,
+                             bool center_ROI);
+  std::pair<int, int> centerROI(int size, int max_size, int step);
+
+  int setFrameRate(bool auto_frame_rate, double frame_rate);
+  int setExposure(bool auto_exposure, double exposure);
+  int setGain(bool auto_gain, double gain);
+  int setShutter(bool auto_shutter, double shutter);
   int printFormat7Capabilities(void);
   int changeMode(std::string mode);
+  int setProperty(const FlyCapture2::PropertyType prop_type, bool on, bool auto_on, double value);
+  FlyCapture2::Property getProperty(const FlyCapture2::PropertyType &prop_type);
+  FlyCapture2::PropertyInfo getPropertyInfo(const FlyCapture2::PropertyType &prop_type);
 };
 
 }  // end of awesomo namespace
