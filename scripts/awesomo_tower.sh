08f2c45572213feb13862dacf747a3f1b17ff82b
Chris Choi <chutsu@gmail.com>
2017-02-20 18:15:35 -0500

    Update awesomo_tower.sh

--- scripts/awesomo_tower.sh
+++ scripts/awesomo_tower.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-tmux new-window -n 'Awesomo Tower'
-
 # launch awesomo
-tmux send-keys "roslaunch awesomo_ros awesomo.launch" C-m
-tmux split-window -h
-tmux send-keys "cd ~/catkin_ws/src/awesomo/scripts/; vim awesomo_remote.py" C-m
+tmux new-window -n 'Awesomo'
+tmux send-keys -t 'Awesomo' 'tx1; roslaunch awesomo_ros awesomo.launch' C-m
+tmux split-window -t 'Awesomo' -h
+tmux send-keys -t 'Awesomo' "cd ~/catkin_ws/src/awesomo/scripts/; vim awesomo_remote.py" C-m
 
-# echo awesomo quadrotor pose
-tmux select-pane -t 0
-tmux split-window -v
-tmux send-keys "sleep 10; rostopic echo /awesomo/quadrotor/pose/local" C-m
-
-# echo dji quadrotor pose
-tmux select-pane -t 1
-tmux split-window -v
-tmux send-keys "sleep 10; rostopic echo /dji_sdk/local_position" C-m
+# # echo awesomo quadrotor pose
+# tmux select-pane -t 0
+# tmux split-window -v
+# tmux send-keys "sleep 10; rostopic echo /awesomo/quadrotor/pose/local" C-m
+#
+# # echo dji quadrotor pose
+# tmux select-pane -t 1
+# tmux split-window -v
+# tmux send-keys "sleep 10; rostopic echo /dji_sdk/local_position" C-m
