11b64aacda3e63c2f4b6003f004d495cf97c4e26
Christopher Choi <chutsu@gmail.com>
2017-06-30 14:37:23 -0400

    Update svo_install.bash

--- scripts/install/svo_install.bash
+++ scripts/install/svo_install.bash
@@ -1,16 +1,26 @@
 #!/bin/sh
 set -e
-ROS_DISTRO="indigo"
-# ROS_DISTRO="kinetic"
+ROS_DISTRO="kinetic"
 CATKIN_WS=$HOME/catkin_ws/
+
+install_dependencies()
+{
+    sudo apt-get install -y \
+        git \
+        cmake \
+        libeigen3-dev
+
+}
 
 install_sophus()
 {
     cd /usr/local/src/
-    git clone https://github.com/strasdat/Sophus.git
+    if [ ! -d Sophus ]; then
+    	git clone https://github.com/strasdat/Sophus.git
+    fi
     cd Sophus
     git checkout a621ff
-    mkdir build
+    mkdir -p build
     cd build
     cmake ..
     make
@@ -19,9 +29,11 @@ install_sophus()
 install_fast()
 {
     cd /usr/local/src/
-    git clone https://github.com/uzh-rpg/fast.git
+    if [ ! -d fast ]; then
+    	git clone https://github.com/uzh-rpg/fast.git
+    fi
     cd fast
-    mkdir build
+    mkdir -p build
     cd build
     cmake ..
     make
@@ -30,9 +42,11 @@ install_fast()
 install_g2o()
 {
     cd /usr/local/src/
-    git clone https://github.com/RainerKuemmerle/g2o.git
+    if [ ! -d g2o ]; then
+        git clone https://github.com/RainerKuemmerle/g2o.git
+    fi
     cd g2o
-    mkdir build
+    mkdir -p build
     cd build
     cmake ..
     make
@@ -42,14 +56,18 @@ install_g2o()
 install_vikit()
 {
     cd $CATKIN_WS/src
-    git clone https://github.com/uzh-rpg/rpg_vikit.git
+    if [ ! -d rpg_vikit ]; then
+        git clone https://github.com/uzh-rpg/rpg_vikit.git
+    fi
 }
 
 install_svo()
 {
     sudo apt-get install ros-$ROS_DISTRO-cmake-modules
     cd $CATKIN_WS/src
-    git clone https://github.com/uzh-rpg/rpg_svo.git
+    if [ ! -d rpg_svo ]; then
+        git clone https://github.com/uzh-rpg/rpg_svo.git
+    fi
 }
 
 
@@ -58,4 +76,4 @@ install_sophus
 install_fast
 install_g2o
 install_vikit
-# install_svo
+install_svo
